405634ad5c92e254093fccf4d72f4a641a21803e
Kim Sedlacek <sedlaceks@optonline.net>
2023-12-27 15:45:02 -0500

    updated to correct naming convention

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -38,55 +38,55 @@ namespace InterfaceExercise
             //Now, create objects of your 3 classes and give their members values.
             //Creatively display and organize their values
 
-            var Car1 = new Car();
-            var Truck1 = new Truck();
-            var SUV1 = new SUV();
+            var car1 = new Car();
+            var truck1 = new Truck();
+            var suv1 = new SUV();
 
-            Car1.Year = "2020";
-            Car1.Make = "Ford";
-            Car1.Model = "Fusion";
-            Car1.HasSunroof = true;
-            Car1.Mileage = 22000;
-            Car1.WarningIndicators = 4;
-            Car1.YearsOpen = 120;
-            Car1.Motto = "Built Ford Tough";
+            car1.Year = "2020";
+            car1.Make = "Ford";
+            car1.Model = "Fusion";
+            car1.HasSunroof = true;
+            car1.Mileage = 22000;
+            car1.WarningIndicators = 4;
+            car1.YearsOpen = 120;
+            car1.Motto = "Built Ford Tough";
 
-            Truck1.Year = "2014";
-            Truck1.Make = "Dodge";
-            Truck1.Model = "Ram 1500";
-            Truck1.DriveTrain = "4WD";
-            Truck1.Color = "red";
-            Truck1.WarningIndicators = 5;
-            Truck1.YearsOpen = 100;
-            Truck1.Motto = "If you ain't first, you're last!.";
+            truck1.Year = "2014";
+            truck1.Make = "Dodge";
+            truck1.Model = "Ram 1500";
+            truck1.DriveTrain = "4WD";
+            truck1.Color = "red";
+            truck1.WarningIndicators = 5;
+            truck1.YearsOpen = 100;
+            truck1.Motto = "If you ain't first, you're last!.";
 
-            SUV1.Year = "2021";
-            SUV1.Make = "Jeep";
-            SUV1.Model = "Grand Cherokee";
-            SUV1.NumberOfSeats = 5;
-            SUV1.Spare = "full size";
-            SUV1.WarningIndicators = 7;
-            SUV1.YearsOpen = 80;
-            SUV1.Motto = "Go Anywhere, Do Anything";
+            suv1.Year = "2021";
+            suv1.Make = "Jeep";
+            suv1.Model = "Grand Cherokee";
+            suv1.NumberOfSeats = 5;
+            suv1.Spare = "full size";
+            suv1.WarningIndicators = 7;
+            suv1.YearsOpen = 80;
+            suv1.Motto = "Go Anywhere, Do Anything";
 
-            Console.WriteLine($"\"{Car1.Year} {Car1.Make} {Car1.Model} For Sale\"\nThe {Car1.Make} {Car1.Model} " +
-                $"is a solid and economical sedan.\nThis {Car1.Year} model has {Car1.Mileage.ToString("N0")} miles, " +
-                $"{Car1.WarningIndicators} warning indicators to keep you safe and yes, it's " +
-                $"{Car1.HasSunroof.ToString().ToLower()}, it has a sun roof! \nThe {Car1.Make} company has been in " +
-                $"business for {Car1.YearsOpen} years, so you know they build a solid car, in fact, their motto is " +
-                $"\"{Car1.Motto}!\"\n");
+            Console.WriteLine($"\"{car1.Year} {car1.Make} {car1.Model} For Sale\"\nThe {car1.Make} {car1.Model} " +
+                $"is a solid and economical sedan.\nThis {car1.Year} model has {car1.Mileage.ToString("N0")} miles, " +
+                $"{car1.WarningIndicators} warning indicators to keep you safe and yes, it's " +
+                $"{car1.HasSunroof.ToString().ToLower()}, it has a sun roof! \nThe {car1.Make} company has been in " +
+                $"business for {car1.YearsOpen} years, so you know they build a solid car, in fact, their motto is " +
+                $"\"{car1.Motto}!\"\n");
 
-            Console.WriteLine($"We have a {Truck1.Year} {Truck1.Make} {Truck1.Model} that we use around our property" +
-                $" to plow snow, haul wood, and move dirt.  Most of the original {Truck1.WarningIndicators} warning " +
-                $"indicators no longer work, but that's okay because the {Truck1.DriveTrain} works great! {Truck1.Make}" +
-                $" makes a great truck, they've been doing it for almost {Truck1.YearsOpen} years and their motto is \"" +
-                $"{Truck1.Motto}\" We would gladly get another {Truck1.Make} {Truck1.Model}, but maybe in a different color so our " +
-                $"kids don't call it \"BIG {Truck1.Color.ToUpper()}\" anymore.\n");
+            Console.WriteLine($"We have a {truck1.Year} {truck1.Make} {truck1.Model} that we use around our property" +
+                $" to plow snow, haul wood, and move dirt.  Most of the original {truck1.WarningIndicators} warning " +
+                $"indicators no longer work, but that's okay because the {truck1.DriveTrain} works great! {truck1.Make}" +
+                $" makes a great truck, they've been doing it for almost {truck1.YearsOpen} years and their motto is \"" +
+                $"{truck1.Motto}\" We would gladly get another {truck1.Make} {truck1.Model}, but maybe in a different color so our " +
+                $"kids don't call it \"BIG {truck1.Color.ToUpper()}\" anymore.\n");
 
-            Console.WriteLine($"My new car isn't brand new, it's a {SUV1.Year} {SUV1.Make} {SUV1.Model}, but so far," +
-                $" I really like it!  It has {SUV1.NumberOfSeats} seats, {SUV1.WarningIndicators} warning indicators " +
-                $"and a {SUV1.Spare} spare tire.  I've had {SUV1.Make}s before and I love them!  They've been around" +
-                $" for over {SUV1.YearsOpen} years and their motto is true, you can \"{SUV1.Motto} in a Jeep!\"");
+            Console.WriteLine($"My new car isn't brand new, it's a {suv1.Year} {suv1.Make} {suv1.Model}, but so far," +
+                $" I really like it!  It has {suv1.NumberOfSeats} seats, {suv1.WarningIndicators} warning indicators " +
+                $"and a {suv1.Spare} spare tire.  I've had {suv1.Make}s before and I love them!  They've been around" +
+                $" for over {suv1.YearsOpen} years and their motto is true, you can \"{suv1.Motto} in a Jeep!\"");
         }
     }
 }
